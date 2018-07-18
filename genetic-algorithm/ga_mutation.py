5cc293baff17013c8e1e53fdf9151c0eeb3cfc8a
André F. Martins <martins.adw@gmail.com>
2018-07-18 15:57:56 -0300

    Ajustar tabulação e final de linha

--- genetic-algorithm/ga_mutation.py
+++ genetic-algorithm/ga_mutation.py
@@ -4,26 +4,26 @@ from ga_gene import Gene
 BIT_INVERSION_MUTATION = 0
 
 def mutation_gene(gene, method, args):
-	new_gene = gene
+    new_gene = gene
 
-	if (method == BIT_INVERSION_MUTATION):
-		new_gene = _bit_inversion_mutation_gene(gene, args)
+    if (method == BIT_INVERSION_MUTATION):
+        new_gene = _bit_inversion_mutation_gene(gene, args)
 
-	return gene
+    return gene
 
 def _bit_inversion_mutation_gene(gene, args):
-	assert 'mutation_chance' in args
+    assert 'mutation_chance' in args
 
-	new_gene = Gene.like(gene)
+    new_gene = Gene.like(gene)
 
-	for name in new_gene.variables:
-		bitsize = new_gene.get_variable_size(name)
+    for name in new_gene.variables:
+        bitsize = new_gene.get_variable_size(name)
 
-		mask = 0
-		for x in range(bitsize):
-			if random.random() < args['mutation_chance']:
-				mask &= (1 << x)
+        mask = 0
+        for x in range(bitsize):
+            if random.random() < args['mutation_chance']:
+                mask &= (1 << x)
 
-		new_gene.variables[name] ^= mask
+        new_gene.variables[name] ^= mask
 
-	return new_gene
+    return new_gene
