d501c914f1c47208f55ea2d775d46bd679ca12b6
André F. Martins <martins.adw@gmail.com>
2019-06-17 17:58:09 -0300

    Calcular a coocorrência dos conceitos por quantidade de conceitos

---
+++ stats/extract.py
@@ -0,0 +1,49 @@
+import pickle
+
+import numpy as np
+
+from acs.instance import Instance
+
+
+if __name__ == '__main__':
+    instance = Instance.load_from_file('instances/real/instance.txt')
+
+    concepts_name = instance.concepts_keys
+
+    num_concepts = len(concepts_name)
+
+    concepts_quant = np.sum(instance.concepts_materials, axis=1)
+    concepts_quant[concepts_quant == 0] = round(np.mean(concepts_quant[concepts_quant != 0]))
+
+    concepts_difficulty = np.empty((num_concepts,))
+    for num, concept in enumerate(instance.concepts_keys):
+        concept_mask = instance.concepts_materials[num]
+        num_materials = concept_mask.sum()
+
+        if num_materials == 0:
+            concepts_difficulty[num] = 0
+        else:
+            concepts_difficulty[num] = np.mean(instance.materials_difficulty[concept_mask])
+    concepts_difficulty[concepts_difficulty == 0] = np.mean(instance.materials_difficulty)
+
+    int_concepts_materials = instance.concepts_materials.astype(int)
+    coocurrence_matrix = int_concepts_materials.dot(int_concepts_materials.T)
+
+    concepts_count = instance.concepts_materials.sum(axis=0)
+    count_histogram = np.bincount(concepts_count)[1:]
+
+    n_coocurrence_matrix = np.empty((count_histogram.shape[0], num_concepts, num_concepts))
+    for i in range(count_histogram.shape[0]):
+        count_mask = (concepts_count == (i + 1))
+
+        int_concepts_materials = instance.concepts_materials[:, count_mask].astype(int)
+        n_coocurrence_matrix[i] = int_concepts_materials.dot(int_concepts_materials.T)
+
+    with open('results/instance_stats.pickle', 'wb') as file:
+        pickle.dump({
+            'concepts_name': concepts_name,
+            'concepts_quant': concepts_quant,
+            'concepts_difficulty': concepts_difficulty,
+            'count_histogram': count_histogram,
+            'n_coocurrence_matrix': n_coocurrence_matrix,
+        }, file)
--- stats/generator.py
+++ stats/generator.py
@@ -1,83 +1,27 @@
 import random
+import pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils.roulette import roulette_spin
+from utils.roulette import Roulette, roulette_spin
 
 
 if __name__ == '__main__':
     ############################################################################
-    num_materials = 1000
+    num_materials = 10000
     mean_concepts = 1.33
     smoothing = 0.01
-
-    concepts_name = [
-          'ICHCC01',   'ICHCC02',   'ICHCC03',   'ICHCC04',   'ICHCC05',   'ICHCC06',
-           'ICSN01',    'ICSN02',    'ICSN03',    'ICSN04',
-            'ICL01',     'ICL02',     'ICL03',
-           'ICFA01',    'ICFA02',    'ICFA03',
-          'ICFBD01',   'ICFBD02',   'ICFBD03',
-           'ICES01',    'ICES02',    'ICES03',
-        'ICFSOOC01', 'ICFSOOC02', 'ICFSOOC03',
-           'ICRC01',    'ICRC02',    'ICRC03',    'ICRC04',    'ICRC05',
-    ]
-
-    concepts_quant = [
-        12, 11, 11, 11, 11, 9,
-        18, 11, 14, 9,
-        13, 15, 19,
-        17, 15, 12,
-        14, 12, 11,
-        11, 12, 13,
-        17, 12, 12,
-        13, 16, 16, 12, 12,
-    ]
-
-    concepts_difficulty = [
-        2.67, 2.73, 3.00, 3.27, 3.36, 2.22,
-        3.06, 3.09, 2.86, 2.78,
-        2.90, 2.53, 3.11,
-        2.76, 2.73, 3.75,
-        3.36, 3.25, 3.00,
-        2.36, 2.50, 2.62,
-        2.71, 2.83, 3.00,
-        2.77, 2.81, 2.94, 3.00, 3.17,
-    ]
-
-    coocurrence_matrix = np.array([
-        [12,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 3, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  9,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0, 18, 10,  9,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0, 10, 11, 10,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  9, 10, 14,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  3,  3,  3,  9,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 15,  7,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  7, 19,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 17,  6,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  6, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 12,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 14,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1, 12,  2,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 11,  8,  4,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  8, 12,  4,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  4, 13,  0,  0,  0,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 17,  9,  1,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9, 12,  1,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1, 12,  0,  0,  0,  0,  0],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 13, 10,  9,  4,  3],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 10, 16, 10,  4,  3],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9, 10, 16,  4,  3],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  4,  4, 12,  7],
-        [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,  3,  3,  7, 12]
-    ], dtype=float)
     ############################################################################
+    with open('results/instance_stats.pickle', 'rb') as file:
+        stats = pickle.load(file)
+
+    concepts_name = stats['concepts_name']
+    concepts_quant = stats['concepts_quant']
+    concepts_difficulty = stats['concepts_difficulty']
+    coocurrence_matrix = np.sum(stats['n_coocurrence_matrix'], axis=0)
+    count_histogram = stats['count_histogram']
+
     num_concepts = len(concepts_name)
     new_concept_rate = 1 - (1 / mean_concepts)
     coocurrence_matrix += smoothing
@@ -85,23 +29,44 @@ if __name__ == '__main__':
 
     materials_list = []
 
-    for i in range(num_materials):
-        new_material = []
+    gen_type = 1
+    if gen_type == 0:
+        for i in range(num_materials):
+            new_material = []
 
-        remaining_concepts_id = list(range(num_concepts))
+            remaining_concepts_id = list(range(num_concepts))
 
-        concept_id = roulette_spin(concepts_quant)
-        new_material.append(remaining_concepts_id.pop(concept_id))
-
-        while random.random() < new_concept_rate and len(new_material) < num_concepts:
-            new_probability = np.sum(coocurrence_matrix[new_material][:, remaining_concepts_id], axis=0)
-
-            concept_id = roulette_spin(new_probability)
+            concept_id = roulette_spin(concepts_quant)
             new_material.append(remaining_concepts_id.pop(concept_id))
 
-        materials_list.append(new_material)
+            while random.random() < new_concept_rate and len(new_material) < num_concepts:
+                new_probability = np.sum(coocurrence_matrix[new_material][:, remaining_concepts_id], axis=0)
 
-    concepts_materials = np.zeros((num_concepts, num_materials))
+                concept_id = roulette_spin(new_probability)
+                new_material.append(remaining_concepts_id.pop(concept_id))
+
+            materials_list.append(new_material)
+    elif gen_type == 1:
+        concept_roulette = Roulette(concepts_quant.astype(float))
+        quant_roulette = Roulette(count_histogram.astype(float))
+
+        for i in range(num_materials):
+            new_material = []
+
+            remaining_concepts_id = list(range(num_concepts))
+            concept_id = concept_roulette.spin()
+            new_material.append(remaining_concepts_id.pop(concept_id))
+
+            quant_concepts = quant_roulette.spin() + 1
+            for j in range(quant_concepts - 1):
+                new_probability = np.sum(coocurrence_matrix[new_material][:, remaining_concepts_id], axis=0)
+
+                concept_id = roulette_spin(new_probability)
+                new_material.append(remaining_concepts_id.pop(concept_id))
+
+            materials_list.append(new_material)
+
+    concepts_materials = np.zeros((num_concepts, num_materials), dtype=int)
     for j, material in enumerate(materials_list):
         for i in material:
             concepts_materials[i, j] = 1
@@ -112,6 +77,18 @@ if __name__ == '__main__':
     print(np.sum(quant_concepts))
     print(np.mean(quant_concepts))
     print(np.std(quant_concepts))
+
+    concepts_count = concepts_materials.sum(axis=0)
+    count_histogram = np.bincount(concepts_count)
+    print(count_histogram / count_histogram.sum())
+    # for i in range(count_histogram.shape[0]):
+    #     count_mask = (concepts_count == i)
+    #
+    #     int_concepts_materials = concepts_materials[:, count_mask].astype(int)
+    #     coocurrence = int_concepts_materials.dot(int_concepts_materials.T)
+    #     print("-%d-------------------------------------------" % i)
+    #     print(coocurrence)
+    #     print("---------------------------------------------")
 
     fig = plt.figure()
     fig.suptitle('Matriz de coocorrência - Sintética - Suavização %.2f' % smoothing)
