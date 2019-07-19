fff5483ce9c79c408530239b0fa77560eba83c8f
André F. Martins <martins.adw@gmail.com>
2019-07-19 17:55:20 -0300

    Selecionar conceitos utilizando coocorrência de múltiplos conceitos

--- stats/extract.py
+++ stats/extract.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import pickle
 
 import numpy as np
@@ -12,8 +13,12 @@ if __name__ == '__main__':
 
     num_concepts = len(concepts_name)
 
+    ############################################################################
+
     concepts_quant = np.sum(instance.concepts_materials, axis=1)
     concepts_quant[concepts_quant == 0] = round(np.mean(concepts_quant[concepts_quant != 0]))
+
+    ############################################################################
 
     concepts_difficulty = np.empty((num_concepts,))
     for num, concept in enumerate(instance.concepts_keys):
@@ -26,11 +31,15 @@ if __name__ == '__main__':
             concepts_difficulty[num] = np.mean(instance.materials_difficulty[concept_mask])
     concepts_difficulty[concepts_difficulty == 0] = np.mean(instance.materials_difficulty)
 
+    ############################################################################
+
     int_concepts_materials = instance.concepts_materials.astype(int)
     coocurrence_matrix = int_concepts_materials.dot(int_concepts_materials.T)
 
     concepts_count = instance.concepts_materials.sum(axis=0)
     count_histogram = np.bincount(concepts_count)[1:]
+
+    ############################################################################
 
     n_coocurrence_matrix = np.empty((count_histogram.shape[0], num_concepts, num_concepts))
     for i in range(count_histogram.shape[0]):
@@ -38,6 +47,43 @@ if __name__ == '__main__':
 
         int_concepts_materials = instance.concepts_materials[:, count_mask].astype(int)
         n_coocurrence_matrix[i] = int_concepts_materials.dot(int_concepts_materials.T)
+
+    ############################################################################
+
+    coocurrence_set = []
+    coocurrence_dict = []
+
+    coocurrence_set.append(defaultdict(set))
+    coocurrence_dict.append({})
+    for i in range(num_concepts):
+        for j in range(i+1, num_concepts):
+            concept_and = instance.concepts_materials[i] & instance.concepts_materials[j]
+            if concept_and.sum() > 0:
+                coocurrence_set[0][i].add(j)
+                coocurrence_set[0][j].add(i)
+                coocurrence_dict[0][frozenset([i, j])] = concept_and
+
+    while (len(coocurrence_dict[-1]) > 0):
+        current_set = defaultdict(set)
+        current_dict = {}
+        for (key, value) in coocurrence_dict[-1].items():
+            # Talvez seja mais rapido calcular quais são as chaves que podem ter coocorrencia
+            # possible_keys = set.intersection(*[coocurrence_set[0][x] for x in key])
+
+            for i in range(num_concepts):
+                if i not in key:
+                    if key.union([i]) in current_dict:
+                        current_set[key].add(i)
+                    else:
+                        concept_and = value & instance.concepts_materials[i]
+                        if concept_and.sum() > 0:
+                            current_set[key].add(i)
+                            current_dict[key.union([i])] = concept_and
+
+        coocurrence_set.append(current_set)
+        coocurrence_dict.append(current_dict)
+
+    ############################################################################
 
     with open('results/instance_stats.pickle', 'wb') as file:
         pickle.dump({
@@ -47,4 +93,6 @@ if __name__ == '__main__':
             'concepts_difficulty': concepts_difficulty,
             'count_histogram': count_histogram,
             'n_coocurrence_matrix': n_coocurrence_matrix,
+            'coocurrence_set': coocurrence_set,
+            'coocurrence_dict': coocurrence_dict,
         }, file)
--- stats/generator.py
+++ stats/generator.py
@@ -1,12 +1,55 @@
 import random
 import pickle
 import math
-from collections import defaultdict
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from utils.roulette import Roulette, roulette_spin
+
+
+def virtual_roulette(size, quant):
+    combinations_size = (math.factorial(size) / math.factorial(size - quant))
+    values_list = [*range(size)]
+
+    combination_value = math.floor(random.random() * combinations_size)
+
+    result = []
+    for i in range(quant):
+        result.append(values_list.pop(combination_value % (size - i)))
+        combination_value //= (size - i)
+
+    return result
+
+
+def select_concepts(stats, quant, roulette_weight=1):
+    num_concepts = len(stats['concepts_name'])
+    concepts_quant = stats['concepts_quant']
+    coocurrence_dict = stats['coocurrence_dict']
+
+    if quant < 1:
+        return []
+    elif quant == 1:
+        return [roulette_spin(concepts_quant)]
+    else:
+        coocurrence = coocurrence_dict[quant-2]
+
+        coocurrence_keys = list(coocurrence.keys())
+        coocurrence_values = [x.sum() for x in coocurrence.values()]
+        coocurrence_quant = sum(coocurrence_values)
+
+        roulette_quant = math.factorial(num_concepts) / (math.factorial(num_concepts - quant) * math.factorial(quant))
+
+        roulette_value = random.random()
+        # print("Chance: %d / (%d + %d) = %f" % (coocurrence_quant, coocurrence_quant, roulette_quant * roulette_weight, (coocurrence_quant / (coocurrence_quant + roulette_quant * roulette_weight))))
+        # print("Valor: %f" % roulette_value)
+
+        if roulette_value < 0.5:
+        # if roulette_value < (coocurrence_quant / (coocurrence_quant + roulette_quant * roulette_weight)):
+            selected_index = roulette_spin(coocurrence_values)
+            return list(coocurrence_keys[selected_index])
+        else:
+            return virtual_roulette(num_concepts, quant)
 
 
 if __name__ == '__main__':
@@ -18,12 +61,14 @@ if __name__ == '__main__':
     with open('results/instance_stats.pickle', 'rb') as file:
         stats = pickle.load(file)
 
-    base_concepts_materials = stats['concepts_materials']
+    concepts_materials = stats['concepts_materials']
     concepts_name = stats['concepts_name']
     concepts_quant = stats['concepts_quant']
     concepts_difficulty = stats['concepts_difficulty']
     coocurrence_matrix = np.sum(stats['n_coocurrence_matrix'], axis=0)
     count_histogram = stats['count_histogram']
+    coocurrence_set = stats['coocurrence_set']
+    coocurrence_dict = stats['coocurrence_dict']
 
     num_concepts = len(concepts_name)
     new_concept_rate = 1 - (1 / mean_concepts)
@@ -68,30 +113,29 @@ if __name__ == '__main__':
                 new_material.append(remaining_concepts_id.pop(concept_id))
 
             materials_list.append(new_material)
+    elif gen_type == 2:
+        quant_roulette = Roulette(count_histogram.astype(float))
 
-    concepts_materials = np.zeros((num_concepts, num_materials), dtype=int)
+        for i in range(num_materials):
+            quant_concepts = quant_roulette.spin() + 1
+
+            materials_list.append(select_concepts(stats, quant_concepts))
+
+    new_concepts_materials = np.zeros((num_concepts, num_materials), dtype=int)
     for j, material in enumerate(materials_list):
         for i in material:
-            concepts_materials[i, j] = 1
+            new_concepts_materials[i, j] = 1
 
-    new_coocurences_matrix = concepts_materials.dot(concepts_materials.T)
-    quant_concepts = np.sum(concepts_materials, axis=0)
+    new_coocurences_matrix = new_concepts_materials.dot(new_concepts_materials.T)
+    quant_concepts = np.sum(new_concepts_materials, axis=0)
 
     print(np.sum(quant_concepts))
     print(np.mean(quant_concepts))
     print(np.std(quant_concepts))
 
-    concepts_count = concepts_materials.sum(axis=0)
+    concepts_count = new_concepts_materials.sum(axis=0)
     count_histogram = np.bincount(concepts_count)
     print(count_histogram / count_histogram.sum())
-    # for i in range(count_histogram.shape[0]):
-    #     count_mask = (concepts_count == i)
-    #
-    #     int_concepts_materials = concepts_materials[:, count_mask].astype(int)
-    #     coocurrence = int_concepts_materials.dot(int_concepts_materials.T)
-    #     print("-%d-------------------------------------------" % i)
-    #     print(coocurrence)
-    #     print("---------------------------------------------")
 
     # fig = plt.figure()
     # fig.suptitle('Matriz de coocorrência - Sintética - Suavização %.2f' % smoothing)
@@ -104,57 +148,3 @@ if __name__ == '__main__':
     # plt.imshow(coocurrence_matrix, interpolation='nearest', cmap='gray')
     # plt.colorbar()
     # plt.show()
-
-    def virtualRoulette(size, quant):
-        combinations_size = (math.factorial(size) / math.factorial(size - quant))
-        values_list = [*range(size)]
-
-        combination_value = math.floor(random.random() * combinations_size)
-
-        result = []
-        for i in range(quant):
-            result.append(values_list.pop(combination_value % (size - i)))
-            combination_value //= (size - i)
-
-        return result
-
-    # print(virtualRoulette(30, 5))
-
-    coocurrence_set = []
-    coocurrence_dict = []
-
-    coocurrence_set.append(defaultdict(set))
-    coocurrence_dict.append({})
-    for i in range(num_concepts):
-        for j in range(i+1, num_concepts):
-            concept_and = base_concepts_materials[i] & base_concepts_materials[j]
-            if concept_and.sum() > 0:
-                coocurrence_set[0][i].add(j)
-                coocurrence_set[0][j].add(i)
-                coocurrence_dict[0][frozenset([i, j])] = concept_and
-
-    while (len(coocurrence_dict[-1]) > 0):
-        current_set = defaultdict(set)
-        current_dict = {}
-        for (key, value) in coocurrence_dict[-1].items():
-            # Talvez seja mais rapido calcular quais são as chaves que podem ter coocorrencia
-            # possible_keys = set.intersection(*[coocurrence_set[0][x] for x in key])
-
-            for i in range(num_concepts):
-                if i not in key:
-                    if key.union([i]) in current_dict:
-                        current_set[key].add(i)
-                    else:
-                        concept_and = value & base_concepts_materials[i]
-                        if concept_and.sum() > 0:
-                            current_set[key].add(i)
-                            current_dict[key.union([i])] = concept_and
-
-        coocurrence_set.append(current_set)
-        coocurrence_dict.append(current_dict)
-
-    print("========================")
-    for n, i in enumerate(coocurrence_dict):
-        print(n+2)
-        for key, value in i.items():
-            print(key, value.sum())
