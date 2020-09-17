75b3b71c5477d640bb875654fe180af09c4a07ae
André F. Martins <martins.adw@gmail.com>
2020-09-17 08:57:21 -0300

    Save extremes result in a file

--- experiments/003_calculate_problem_extremes.py
+++ experiments/003_calculate_problem_extremes.py
@@ -1,3 +1,8 @@
+import datetime
+import os
+import pickle
+import sys
+
 from read.algorithm import create_results_name_list
 from process.extremes import get_instances_extremes
 
@@ -8,8 +13,24 @@ algorithms_multi = ['nsga_ii']
 num_objectives_list = [2, 3]
 
 results_name_list = create_results_name_list(instances, algorithms_single, algorithms_multi, num_objectives_list)
-print(results_name_list)
-(worst_point, nondominated_population) = get_instances_extremes(results_name_list)
+extremes_dict = get_instances_extremes(results_name_list)
 
-print('Worst point:', worst_point)
-print('Nondominated population:', nondominated_population)
+extremes_path = 'results/extremes'
+for ((instance_name, num_objectives), extremes) in extremes_dict.items():
+    extremes_file = os.path.join(extremes_path, "%s_%d.pickle" % (instance_name, num_objectives))
+    results = {
+        'info': {
+            'command': os.path.basename(sys.argv[0]) + " " + " ".join(sys.argv[1:]),
+            'datetime': str(datetime.datetime.now()),
+            'extremes_path': extremes_path,
+            'extremes_file': extremes_file,
+            'results_name_list': results_name_list,
+            'instance_name': instance_name,
+            'num_objectives': num_objectives,
+            'data_description': '{worst_point: students[(objectives)], nondominated_population: students[(individual, objectives)]}',
+        },
+        'data': extremes,
+    }
+
+    with open(extremes_file, 'wb') as file:
+        pickle.dump(results, file)
--- process/extremes.py
+++ process/extremes.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from acs.objective import reduce_objectives
 from utils.multiobjective import sort_nondominated
 from read.algorithm import open_results, get_results_name, get_results_best_all_objectives
 
@@ -14,18 +15,18 @@ Gerar a melhor frente de pareto
     Conjunto de individuos não dominados entre todas as soluções
 '''
 
-# return = (worst_point, nondominated_population)
-# worst_point = {instance: students[(objectives)]}
-# nondominated_population = {instance: students[(individual, objectives)]}
-def get_instances_extremes(instances_results_name, worst_point=None, nondominated_population=None):
-    if worst_point is None:
-        worst_point = dict()
-    if nondominated_population is None:
-        nondominated_population = dict()
+# return = {(instance_name, num_objectives): {worst_point, nondominated_population}}
+# worst_point = students[(objectives)]
+# nondominated_population = students[(individual, objectives)]
+def get_instances_extremes(instances_results_name, extremes=None):
+    if extremes is None:
+        extremes = dict()
+
+    num_objectives_list = [2, 3, 4, 5]
 
     for (instance_name, results_name_list) in instances_results_name.items():
-        worst_point[instance_name] = None  # students[(num_objectives)]
-        nondominated_population[instance_name] = None  # students[(num_individuals, num_objectives)]
+        # worst_point[(instance_name, 5)] = None  # students[(num_objectives)]
+        # nondominated_population[(instance_name, 5)] = None  # students[(num_individuals, num_objectives)]
         for results_name_info in results_name_list:
             results_name = get_results_name(*results_name_info)
             results = open_results(results_name)
@@ -40,21 +41,26 @@ def get_instances_extremes(instances_results_name, worst_point=None, nondominate
             num_individuals = best_population.shape[2]
             num_objectives = best_population.shape[3]
 
-            if worst_point[instance_name] is None:
-                worst_point[instance_name] = [None] * num_students
-            if nondominated_population[instance_name] is None:
-                nondominated_population[instance_name] = [None] * num_students
+            for num_objectives in num_objectives_list:
+                if (instance_name, num_objectives) not in extremes:
+                    extremes[(instance_name, num_objectives)] = {
+                        'worst_point': [None] * num_students,
+                        'nondominated_population': [None] * num_students,
+                    }
 
-            for i in range(num_students):
-                # (repetitions, individuals, MAX_OBJECTIVES)
-                problem_best_population = best_population[:, i, :, :]
-                # (repetitions * individuals, MAX_OBJECTIVES)
-                problem_best_population = problem_best_population.reshape(problem_best_population.shape[0] * problem_best_population.shape[1], problem_best_population.shape[2])
+                instance_extremes = extremes[(instance_name, num_objectives)]
+                for i in range(num_students):
+                    # (repetitions, individuals, MAX_OBJECTIVES)
+                    problem_best_population = best_population[:, i, :, :]
+                    # (repetitions * individuals, MAX_OBJECTIVES)
+                    problem_best_population = problem_best_population.reshape(problem_best_population.shape[0] * problem_best_population.shape[1], problem_best_population.shape[2])
+                    # (repetitions * individuals, num_objectives)
+                    problem_best_population = np.apply_along_axis(reduce_objectives, 1, problem_best_population, num_objectives)
 
-                worst_point[instance_name][i] = get_worst_point(problem_best_population, base=worst_point[instance_name][i])
-                nondominated_population[instance_name][i] = get_nondominated_population(problem_best_population, base=nondominated_population[instance_name][i])
+                    instance_extremes['worst_point'][i] = get_worst_point(problem_best_population, base=instance_extremes['worst_point'][i])
+                    instance_extremes['nondominated_population'][i] = get_nondominated_population(problem_best_population, base=instance_extremes['nondominated_population'][i])
 
-    return (worst_point, nondominated_population)
+    return extremes
 
 
 # population = (individuals, objectives)
